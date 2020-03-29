22c49dfcd064e8522bcafdc30cd420b4007bc0e5
Alex Stevens <zandra630@gmail.com>
2020-03-29 15:43:02 -0500

    Adds page route for viewing one search result

--- components/SearchResultCard.js
+++ components/SearchResultCard.js
@@ -1,27 +1,38 @@
-import Card from 'react-bootstrap/Card';
+import { Fragment } from 'react'
+import Link from 'next/link'
+import Card from 'react-bootstrap/Card'
 
-const cardStyle = {
+const styles = {
+  card: {
   maxHeight: 200,
   margin: 5,
-  overflow: "auto"
+  overflow: "auto",
+  }
 }
 
 const SearchResultCard = (props) => (
-  <Card className="flex-row" style={cardStyle}>
-      <Card.Img variant="left" src={props.image}  alt={props.title} />
-    <Card.Body>
-      <Card.Title>              
-        {props.title}  
-          <Card.Subtitle className="mb-2 text-muted">
-            {props.authors}
-          </Card.Subtitle>
-      </Card.Title>
-      <Card.Text style={cardStyle}>
-        {props.description}
-      </Card.Text>
-        <small className="text-muted">{props.publisher} {props.publishedDate} ({props.pageCount} pages)</small>
-    </Card.Body>
-  </Card>
+  <Fragment>
+    <Card className="flex-row" style={styles.card}>
+        <Card.Img variant="left" src={props.image}  alt={props.title} />
+      <Card.Body>
+        <Card.Title>              
+          {props.title}  
+            <Card.Subtitle className="mb-2 text-muted">
+              {props.authors}
+            </Card.Subtitle>
+        </Card.Title>
+        <Card.Text>
+          {props.description}
+        </Card.Text>
+        <Card.Text className="d-flex justify-content-between align-items-baseline mt-3">
+          <small className="text-muted">{props.publisher} {props.publishedDate} ({props.pageCount} pages) id:{props.id}</small>
+          <Link href="/search/[id]" as={`/search/${props.id}`}>
+            <a className="ml-5 font-italic">See More</a>
+          </Link>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </Fragment>
   );
 
 export default SearchResultCard;
--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,6 @@ export default class MyDocument extends Document {
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" />  
           <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" />  
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" />
-          
         </Head>
         <body>
           <Main />
